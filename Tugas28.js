9fea84adee3931208c5d3f72bb4c7864d4d4d279
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-11-05 22:39:45 +0700

    RegExp exec dan match /[A-E]/gi
    RegExp exec dan match /[A-E]/gi

---
+++ Tugas28.js
@@ -0,0 +1,9 @@
+function Regex() {
+    var str = "abcdefghijklmnopqrstuvwxyz";
+
+    console.log(str);
+    console.log("===============================================================");
+    var cari = str.search(/s/);
+    console.log(cari);
+}
+Regex();
